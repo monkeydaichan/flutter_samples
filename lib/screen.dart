14499caad7944efa784e7d468d76ea756cee8af5
monkeydaichan <monkeydaichi124@gmail.com>
2020-04-15 13:36:04 -0400

    create dismissKeyboard

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,67 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flutter_samples/widgets.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_samples/bloc.dart';
 
-class DatetimePage extends StatelessWidget {
-  DatetimePage({Key key}) : super(key: key);
+class MainPage extends StatelessWidget {
+  MainPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Theme.of(context).primaryColor,
-        elevation: 0.0,
-      ),
-      body: ChangeNotifierProvider<TimeStore>(
-        create: (_) => TimeStore(),
-        child: DateTimeWidget(),
-      ),
-    );
+    return GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(),
+          body: MainPageWidgets(),
+        ));
   }
 }
 
-class DateTimeWidget extends StatelessWidget {
-  DateTimeWidget({Key key, this.title}) : super(key: key);
+class MainPageWidgets extends StatelessWidget {
+  MainPageWidgets({Key key, this.title}) : super(key: key);
   final String title;
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Container(
-            child: Row(
-              children: <Widget>[
-                Icon(FontAwesomeIcons.clock),
-                Padding(
-                    padding: EdgeInsets.only(left: 24.0),
-                    child: Text(
-                      "日時",
-                      style: Theme.of(context).textTheme.bodyText1,
-                    )),
-                Expanded(
-                  child: Stack(
-                      alignment: Alignment.centerRight,
-                      children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            BasicDateField(),
-                            TimeShow(),
-                          ],
-                        )
-                      ]),
-                ),
-              ],
-            ),
-          ),
-        ),
-        TimeField(),
-      ],
+        child: Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Container(
+          child: TextField(
+              autofocus: true,
+              style: Theme.of(context).textTheme.bodyText1,
+              textAlignVertical: TextAlignVertical.center,
+              textAlign: TextAlign.left,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                border: new OutlineInputBorder(borderSide: new BorderSide()),
+                hintText: title,
+                hintStyle: Theme.of(context).textTheme.bodyText1,
+              ))),
     ));
   }
 }
