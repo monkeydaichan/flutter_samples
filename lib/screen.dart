3d351fd3af07c829322118362ee52893b6868d6a
monkeydaichan <monkeydaichi124@gmail.com>
2020-04-20 00:28:57 -0400

    create stepProgress

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class MainPage extends StatelessWidget {
-  MainPage({Key key}) : super(key: key);
-
+  MainPage({Key key, this.stepNo}) : super(key: key);
+  final int stepNo;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -14,31 +15,163 @@ class MainPage extends StatelessWidget {
         },
         child: Scaffold(
           appBar: AppBar(),
-          body: MainPageWidgets(),
+          body: (stepNo == 1)
+              ? Container1()
+              : (stepNo == 2) ? Container2() : Container0(),
         ));
   }
 }
 
-class MainPageWidgets extends StatelessWidget {
-  MainPageWidgets({Key key, this.title}) : super(key: key);
-  final String title;
+class Container0 extends StatelessWidget {
+  Container0({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final int currentStep = 0;
     return Container(
-        child: Padding(
+        child: Column(
+      children: <Widget>[
+        StepProgress(currentStep: currentStep),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Container(
+              child: Center(
+                  child: Text(
+            "Container0",
+            style: Theme.of(context).textTheme.bodyText1,
+          ))),
+        ),
+        FloatingActionButton(
+          child: Icon(
+            Icons.chevron_right,
+            size: 36,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return MainPage(
+                    stepNo: currentStep + 1,
+                  );
+                },
+              ),
+            );
+          },
+        )
+      ],
+    ));
+  }
+}
+
+class Container1 extends StatelessWidget {
+  Container1({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final int currentStep = 1;
+    return Container(
+        child: Column(
+      children: <Widget>[
+        StepProgress(currentStep: currentStep),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Container(
+              child: Center(
+                  child: Text(
+            "Container1",
+            style: Theme.of(context).textTheme.bodyText1,
+          ))),
+        ),
+        FloatingActionButton(
+          child: Icon(
+            Icons.chevron_right,
+            size: 36,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return MainPage(
+                    stepNo: currentStep + 1,
+                  );
+                },
+              ),
+            );
+          },
+        )
+      ],
+    ));
+  }
+}
+
+class Container2 extends StatelessWidget {
+  Container2({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final int currentStep = 2;
+    return Container(
+        child: Column(
+      children: <Widget>[
+        StepProgress(currentStep: currentStep),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Container(
+              child: Center(
+                  child: Text(
+            "Container2",
+            style: Theme.of(context).textTheme.bodyText1,
+          ))),
+        ),
+      ],
+    ));
+  }
+}
+
+class StepProgress extends StatelessWidget {
+  StepProgress({Key key, this.currentStep}) : super(key: key);
+  final int currentStep;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
       padding: EdgeInsets.all(16.0),
       child: Container(
-          child: TextField(
-              autofocus: true,
-              style: Theme.of(context).textTheme.bodyText1,
-              textAlignVertical: TextAlignVertical.center,
-              textAlign: TextAlign.left,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                border: new OutlineInputBorder(borderSide: new BorderSide()),
-                hintText: title,
-                hintStyle: Theme.of(context).textTheme.bodyText1,
-              ))),
-    ));
+        child: StepProgressIndicator(
+          totalSteps: 3,
+          currentStep: currentStep,
+          size: 36,
+          selectedColor: Colors.black,
+          unselectedColor: Colors.grey[200],
+          customStep: (index, color, _) => color == Colors.black
+              ? Container(
+                  decoration: new BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.white,
+                  ),
+                )
+              : (index == currentStep)
+                  ? Container(
+                      decoration: new BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                    )
+                  : Container(
+                      decoration: new BoxDecoration(
+                        color: color,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.remove,
+                      ),
+                    ),
+        ),
+      ),
+    );
   }
 }
